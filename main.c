fb1c6a7dbed3388cd2fbec25b66105f519f27ff5
ilyamironow <miilig@yandex.ru>
2021-11-23 02:30:54 +0300

    Passed

--- main.c
+++ main.c
@@ -1,48 +1,128 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <wait.h>
-#include <malloc.h>
-
 #include "ipc.h"
 #include "common.h"
 #include "pa1.h"
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct self_structure_type {
-    int i; // current process's number
+    int id; // current process's number
     int N; // number of processes
-    int ****fd;
+    int ***fd;
 };
 
 int send_multicast(void *self, const Message *msg) {
-    for (int i = 0; i < (*(struct self_structure_type *) self).N; i++) {
-        if ((*(struct self_structure_type *) self).i == i)
+    struct self_structure_type *str = (struct self_structure_type *) self;
+
+    for (int i = 0; i < str->N; i++) {
+        if (str->id == i)
             continue;
-        write((*((*(struct self_structure_type *) self).fd))[(*(struct self_structure_type *) self).i][i][1], msg,
-              sizeof(Message));
+        write(
+                str->fd[str->id][i][1],
+                msg,
+                sizeof(Message));
     }
     return 0;
 }
 
 int receive_any(void *self, Message *msg) {
-    for (int i = 1; i < (*(struct self_structure_type *) self).N; i++) {
-        if ((*(struct self_structure_type *) self).i == i)
+    struct self_structure_type *str = (struct self_structure_type *) self;
+
+    for (int i = 1; i < str->N; i++) {
+        if (str->id == i)
             continue;
 
-        Message process_msg;
-        read((*((*(struct self_structure_type *) self).fd))[i][(*(struct self_structure_type *) self).i][0],
-             &process_msg, sizeof(Message));
+        read(
+                str->fd[i][str->id][0],
+                msg,
+                sizeof(Message));
     }
     return 0;
 }
 
-int main(__attribute__((unused)) int argc, char **argv) {
-    FILE *f, *f1;
-    f = fopen(events_log, "a+"); // a+ (create + append) option will allow appending which is useful in a log file
-    f1 = fopen(pipes_log, "a+");
-    int N = (int) (*argv[2] - '0');
-    N++;
+void close_pipes(int N, int ***fd) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (fd[i][j][0] > 0) {
+                close(fd[i][j][0]);
+                fd[i][j][0] = -123;
+            }
+            if (fd[i][j][1] > 0) {
+                close(fd[i][j][1]);
+                fd[i][j][1] = -123;
+            }
+        }
+    }
+}
+
+void child(FILE *events_log_file, int N, int ***fd, int i) {
+    for (int q = 0; q < N; q++) {
+        for (int j = 0; j < N; j++) {
+            if (q != i) {
+                if (fd[j][q][0] > 0) {
+                    close(fd[j][q][0]);
+                    fd[j][q][0] = -1;
+                }
+                if (fd[q][j][1] > 0) {
+                    close(fd[q][j][1]);
+                    fd[q][j][1] = -1;
+                }
+            }
+        }
+    }
+
+    printf(log_started_fmt, i, getpid(), getppid()); //* пишет в лог started
+    fprintf(events_log_file, log_started_fmt, i, getpid(), getppid()); //* пишет в лог started
+
+    struct self_structure_type self_structure = {.id = (int) i, .N = (int) N, .fd = fd};
+
+    // отправляет started всем процессам
+    Message msg;
+    MessageHeader s_header = {.s_magic = MESSAGE_MAGIC, .s_type = STARTED, .s_local_time = (int16_t) time(NULL)};
+    s_header.s_payload_len = snprintf(msg.s_payload, MAX_PAYLOAD_LEN, log_started_fmt, i, getpid(), getppid());
+    msg.s_header = s_header;
+    send_multicast(&self_structure, &msg);
+
+    // дождаться от других дочерних сообщения STARTED
+    receive_any(&self_structure, &msg);
+    printf(log_received_all_started_fmt, i); //* пишет в лог received all started
+    fprintf(events_log_file, log_received_all_started_fmt, i); //* пишет в лог received all started
+
+    printf(log_done_fmt, i); //* пишет в лог done
+    fprintf(events_log_file, log_done_fmt, i); //* пишет в лог done
+
+    // отправляет DONE всем
+    s_header.s_type = DONE;
+    s_header.s_payload_len = snprintf(msg.s_payload, MAX_PAYLOAD_LEN, log_done_fmt, i);
+    s_header.s_local_time = (int16_t) time(NULL);
+    msg.s_header = s_header;
+    send_multicast(&self_structure, &msg);
+
+    // дождаться от других дочерних сообщения done
+    receive_any(&self_structure, &msg);
+    printf(log_received_all_done_fmt, i); //* пишет в лог received all started
+    fprintf(events_log_file, log_received_all_done_fmt, i); //* пишет в лог received all started
+
+    close_pipes(N, fd);
+    fclose(events_log_file);
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    FILE *events_log_file, *pipes_log_file;
+    events_log_file = fopen(events_log,
+                            "a+"); // a+ (create + append) option will allow appending which is useful in a log file
+    pipes_log_file = fopen(pipes_log, "a+");
+    int N = 0;
+    for (int i = 0; i < argc; i++)
+        if (!strcmp(argv[i], "-p")) {
+            N = strtol(argv[i + 1], &argv[i + 1], 0);
+            N++;
+            break;
+        }
 
     // fd['from']['to'][0/1]
     int ***fd = (int ***) malloc(N * sizeof(int **));
@@ -53,117 +133,64 @@ int main(__attribute__((unused)) int argc, char **argv) {
             fd[i][j] = (int *) malloc(2 * sizeof(int));
     }
 
-    for (int i = 1; i < N; i++)
-        for (int j = 0; j < i; j++) {
-            pipe(fd[i][j]);
-            printf("fd[%d][%d][0] fd[%d][%d][1]\n", i, j, i, j);
-            fprintf(f1, "fd[%d][%d][0] fd[%d][%d][1]\n", i, j, i, j);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) {
+            if (i != j) {
+                pipe(fd[i][j]);
+                fprintf(pipes_log_file, "fd[%d][%d][0] fd[%d][%d][1]\n", i, j, i, j);
+            } else {
+                fd[i][j][0] = -123;
+                fd[i][j][1] = -123;
+            }
         }
+    fclose(pipes_log_file);
 
-    for (int j = 1; j < N; j++)
-        for (int i = 0; i < j; i++) {
-            pipe(fd[i][j]);
-            printf("fd[%d][%d][0] fd[%d][%d][1]\n", i, j, i, j);
-            fprintf(f1, "fd[%d][%d][0] fd[%d][%d][1]\n", i, j, i, j);
-        }
-    fclose(f1);
     /*
      * We create the pipe before forking the processes, so they share the pipe fds.
      * If we swap these two lines, both processes create their own pipe,
      * and they have different values, thus they can't communicate using fd!
      */
-    int p[N];
-    p[0] = -1;
     //starts from 1 for less confusion because 0 is a parent
-    // p[0] = -1 is for the main cycle
     for (int i = 1; i < N; i++) {
-        p[i] = fork();
-        if (p[i] == 0)
-            break;
+        if (fork() == 0) {
+            child(events_log_file, N, fd, i);
+        }
     }
 
-    /* mail cycle */
-    for (int i = N - 1; i >= 0; i--)
-        if (p[i] == 0) {
-            //in child process p[i]
-            fprintf(f, log_started_fmt, i, getpid(), getppid()); //* пишет в лог started
-
-            char buffer[50];
-            int len;
-            len = sprintf(buffer, log_started_fmt, i, getpid(), getppid());
-
-            struct self_structure_type self_structure = {.i = i, .N = N, .fd = &fd};
-
-            for (int j = 0; j < N; j++) {
-                if (j == i)
-                    continue;
-                close(fd[i][j][0]); // close read end for pipe direction from current process
-                close(fd[j][i][1]); // close write end for pipe direction to current process
+    for (int q = 0; q < N; q++) {
+        for (int j = 0; j < N; j++) {
+            if (q != PARENT_ID) {
+                if (fd[j][q][0] > 0) {
+                    close(fd[j][q][0]);
+                    fd[j][q][0] = -1;
+                }
+                if (fd[q][j][1] > 0) {
+                    close(fd[q][j][1]);
+                    fd[q][j][1] = -1;
+                }
             }
-            // отправляет started всем процессам
-            MessageHeader s_header = {.s_magic = MESSAGE_MAGIC, .s_payload_len = len, .s_type = STARTED, .s_local_time = (int16_t) time(
-                    NULL)};
-            Message msg = {.s_header = s_header, .s_payload = buffer};
-            send_multicast(&self_structure, &msg);
-
-            // дождаться от других дочерних сообщения STARTED
-            receive_any(&self_structure, &msg);
-            fprintf(f, log_received_all_started_fmt, i); //* пишет в лог received all started
-
-            fprintf(f, log_done_fmt, i); //* пишет в лог done
-            // отправляет DONE всем
-            s_header.s_type = DONE;
-            len = sprintf(buffer, log_done_fmt, i);
-            s_header.s_payload_len = len;
-            s_header.s_local_time = (int16_t) time(NULL);
-            msg = (Message) {.s_header = s_header, .s_payload = buffer};
-            send_multicast(&self_structure, &msg);
-
-            // дождаться от других дочерних сообщения done
-            receive_any(&self_structure, &msg);
-            fprintf(f, log_received_all_done_fmt, i); //* пишет в лог received all started
-            //тут и выше пишет received даже если не received
-
-            for (int j = 0; j < N; j++) {
-                if (j == i)
-                    continue;
-                close(fd[i][j][1]); // close read end for pipe direction from current process
-                close(fd[j][i][0]); // close write end for pipe direction to current process
-            }
-            break;
-        } else if (i == 0) {
-            //in parent process
-            struct self_structure_type self_structure = {.i = i, .N = N, .fd = &fd};
-
-            for (int j = 1; j < N; j++) {
-                close(fd[i][j][0]); // close read end for pipe direction from current process
-                close(fd[j][i][1]); // close write end for pipe direction to current process
-            }
-            MessageHeader s_header = {.s_type = STARTED};
-            Message msg = {.s_header = s_header};
-            receive_any(&self_structure, &msg);
-            fprintf(f, log_received_all_started_fmt, i); //* пишет в лог received all started
-
-            s_header.s_type = DONE;
-            msg.s_header = s_header;
-            receive_any(&self_structure, &msg);
-            fprintf(f, log_received_all_done_fmt, i); //* пишет в лог received all done
-
-            for (int j = 1; j < N; j++) {
-                close(fd[i][j][1]); // close read end for pipe direction from current process
-                close(fd[j][i][0]); // close write end for pipe direction to current process
-            }
-
-            while (wait(NULL) > 0);
-
-            break;
-            /*
-             * wait for state changes in a child of the calling process
-             * state change is considered to be: the child terminated; the child was stopped by a signal;
-             * or the child was resumed by a signal.S
-             * if a wait is not performed, then the terminated child remains in a "zombie" state
-             */
         }
+    }
+
+    //in parent process
+    struct self_structure_type self_structure = {.id = (int) 0, .N = (int) N, .fd = fd};
+    Message msg;
+    //wait started
+    receive_any(&self_structure, &msg);
+    //wait done
+    receive_any(&self_structure, &msg);
+
+    while (wait(NULL) > 0);
+
+    close_pipes(N, fd);
+
+    /*
+     * wait for state changes in a child of the calling process
+     * state change is considered to be: the child terminated; the child was stopped by a signal;
+     * or the child was resumed by a signal.S
+     * if a wait is not performed, then the terminated child remains in a "zombie" state
+     */
+    fclose(events_log_file);
 
     // deallocate memory
     for (int i = 0; i < N; i++) {
@@ -173,7 +200,5 @@ int main(__attribute__((unused)) int argc, char **argv) {
         free(fd[i]);
     }
     free(fd);
-
-    fclose(f);
     return 0;
 }
